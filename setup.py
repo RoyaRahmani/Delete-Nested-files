23ca411a148b075f9e2e29435f299834a8715b44
Roya <roya.rahmani1994@gmail.com>
2023-06-19 14:14:43 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 import os
 import shutil
 
-main_directory = 'SGB_genome_fastas_part2'
-new_directory = 'SGB_genome_fastas_partAA'
+#  Provide the path to the nested file.
+main_directory = 'path/to/main/directory'
 
+# Specify the desired location for saving your new file.
+new_directory = 'path/to/new/directory'
+
+# Make sure to create the new directory if it does not already exist.
 if not os.path.exists(new_directory):
     os.makedirs(new_directory)
 
-
-fasta_files = []
-
+#  Iterate through the folders in the main directory
 for folder in os.listdir(main_directory):
-    folder_path = os.path.join(main_directory,folder)
     
+    folder_path = os.path.join(main_directory,folder)
     if os.path.isdir(folder_path):
-
+        
+        # Iterate through the files in the folder
         for file in os.listdir(folder_path):
             file_path = os.path.join(folder_path,file)
+            
+            # Verify if the file format is as expected.
             if file.endswith('.fa'):
-                fasta_files.append(file_path)
+                
+                # Copy the file to the new directory
                 shutil.copy(file_path,new_directory)
